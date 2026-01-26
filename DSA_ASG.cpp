3160ecd31c7d1ce85bd1f58d083f1975614322ef
Shakir31 <s10269080@connect.np.edu.sg>
2026-01-26 14:41:35 +0800

    added helper functions in DSA_ASG.cpp

--- DSA_ASG.cpp
+++ DSA_ASG.cpp
@@ -9,7 +9,7 @@
 #include "Review.h"
 using namespace std;
 
-// Global storage
+//global storage
 const int MAX_GAMES = 1000;
 const int MAX_MEMBERS = 100;
 const int MAX_RECORDS = 1000;
@@ -24,7 +24,34 @@ int recordCount = 0;
 int reviewCount = 0;
 HashTable gameHash;
 
-#include <iostream>
+//HELPER FUNCTIONS
+
+//helper function to get current date as string
+string getCurrentDate() {
+    return "2026-01-18";
+}
+
+//helper function to clear input buffer
+void clearInputBuffer() {
+    cin.clear();
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+}
+
+//helper function to pause screen
+void pauseScreen() {
+    cout << "\nPress Enter to continue...";
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+}
+
+//function to find member by ID
+int findMember(string memberID) {
+    for (int i = 0; i < memberCount; i++) {
+        if (members[i].getMemberID() == memberID) {
+            return i;
+        }
+    }
+    return -1;
+}
 
 int main()
 {
